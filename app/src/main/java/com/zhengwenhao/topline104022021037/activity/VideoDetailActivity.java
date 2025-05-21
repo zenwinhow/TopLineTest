3153fd7f6e4fb5e7c0e13af1d3fb46ad13c1721f
Wenhao Zheng <zenwinhow@outlook.com>
2025-05-21 15:58:41 +0800

    add video detail activity(to be done)

---
+++ app/src/main/java/com/zhengwenhao/topline104022021037/activity/VideoDetailActivity.java
@@ -0,0 +1,20 @@
+package com.zhengwenhao.topline104022021037.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.zhengwenhao.topline104022021037.R;
+
+public class VideoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_detail);
+    }
+}
